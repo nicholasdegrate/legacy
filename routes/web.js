e09c11eeaa1b749db55a8d70b6361b360e9e1498
Nicholas Degrate <nicholasdegrate@Nicholass-Mac-mini.local>
2020-06-20 11:14:55 -0400

    fixing client issue

--- routes/web.js
+++ routes/web.js
@@ -34,7 +34,7 @@ router.post('/send', (req,res) => {
       // create reusable transporter object using the default SMTP transport
       let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 587,
+        port: 465,
         secure: true, // true for 465, false for other ports
         auth: {
           user: 'legacyrebuildproperties@gmail.com', // generated ethereal user
